ab93e0da79dd772e14c7899c577b6346832086a4
hazemghaly <hazemghaly30@gmail.com>
2023-08-09 02:19:48 +0000

    lower case alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-ch = map( chr, range( ord ('a'), ord ('z') + 1 ) )
+ch = map(chr, range(ord('a'), ord('z') + 1))
 for alph in ch:
-   print(alph.format(ch), end=" ")
+    print(alph.format(ch), end=" ")
