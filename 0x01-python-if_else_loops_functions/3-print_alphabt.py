341d7ae2b7c1f63255bca3abe24fb094f2117ed5
hazemghaly <hazemghaly30@gmail.com>
2023-08-09 02:40:11 +0000

    lower case alphawithout e +q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 ch = map(chr, range(ord('a'), ord('d') + 1))
 for alph in ch:
-    print(alph.format(ch), end="")
-ch = map(chr, range(ord('f'), ord('p') + 1))
-for alph in ch:
-    print(alph.format(ch), end="")
-ch = map(chr, range(ord('r'), ord('z') + 1))
-for alph in ch:
-    print(alph.format(ch), end="")
+    if alph not in 'eq':
+        print(alph.format(ch), end="")
