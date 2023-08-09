82f887ee50c69bfdaa39fc662311ec8c47bf8940
hazemghaly <hazemghaly30@gmail.com>
2023-08-09 17:32:21 +0000

    convert to upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for s in str:
+for s in str:
         if s in map(chr, range(ord('a'), ord('z') + 1)):
             s = chr(ord(s) - 32)
+            print("{}".format(s), end="")
+        elif s >= 'A' and s <= 'Z' or s == " " or ord(s) in range (49, 59) :
             print("{}".format(s), end="")
     print("")
