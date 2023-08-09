1e397bbc7432f9f0b797062e226ff01ff2e339d3
hazemghaly <hazemghaly30@gmail.com>
2023-08-09 18:40:46 +0000

    lists.h update

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 def islower(c):
+def magic_calculation(a, b, c):
+def remove_char_at(str, n):
+listint_t *insert_node(listint_t **head, int number);
+def fizzbuzz():
+def pow(a, b):
+def add(a, b):
 def uppercase(str):
 def print_last_digit(number):
 #endif
