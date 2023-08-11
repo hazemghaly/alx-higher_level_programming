4e331ea7a3aad55803ed905558633e00f18d8929
hazemghaly <hazemghaly30@gmail.com>
2023-08-11 01:50:25 +0000

    calc Arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,8 @@ if __name__ == '__main__':
     n = len(sys.argv) - 1
     x = 0
     for s in range(1, n + 1):
-        x = x + int(sys.argv[s])
+        if sys.argv[s].isnumeric():
+           if int(sys.argv[s]) < 0:
+                x = int(sys.argv[s])
+        x += int(sys.argv[s])
     print("{}".format(x))
