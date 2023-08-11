f9c68794f33c33376d2f702073fcff50d60973fb
hazemghaly <hazemghaly30@gmail.com>
2023-08-11 18:07:23 +0000

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
     import sys
     n = len(sys.argv)
     if n != 4:
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-        exit(1)
-    operator = str(sys.argv[2])
-    if sys.argv[1].isnumeric() and sys.argv[3].isnumeric():
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-    print(operator)
-    if operator not in ['+', '-', '*', '/']:
+        sys.exit(1)
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and / ")
-        exit(1)
-    from calculator_1 import add, sub, mul, div
-    if operator == '+':
-        print('{} {} {} = {}'.format(a, operator, b, add(a, b)))
-    if operator == '-':
-        print('{} {} {} = {}'.format(a, operator, b, sub(a, b)))
-    if operator == '*':
-        print('{} {} {} = {}'.format(a, b, mul(a, b)))
-    if operator == '/':
-        print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
+        sys.exit(1)
+    print('{} {} {} = {}'.format(a, operator, b, ops[operator](a, b)))
