898706a0881ba1bfb1394f2209c3b57dfa078ce3
hazemghaly <hazemghaly30@gmail.com>
2023-08-12 23:49:40 +0000

    function edit in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return (my_list)
-    cpy_list = my_list.copy()
+    cpy_list = []
+    for s in range(len(my_list)):
+        cpy_list.append(my_list[s])
     cpy_list[idx] = element
     return (cpy_list)
