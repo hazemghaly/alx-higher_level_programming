8f0c670a9b1d0f552170226d51015087f353c21f
hazemghaly <hazemghaly30@gmail.com>
2023-08-12 22:40:13 +0000

    function divisable element of list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     for i in range(0, len(my_list) + 1):
-        print(my_list[i])
         if my_list[i] % 2 != 0:
             return (i, False)
     return (i, True)
