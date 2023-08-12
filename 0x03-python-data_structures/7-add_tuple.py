c867da7a467dd9d80d46bd8939753e5cc7884ffb
hazemghaly <hazemghaly30@gmail.com>
2023-08-12 23:16:46 +0000

    function add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     a = []
-    if len(tuple_a) == len(tuple_b):
-        for i in range(0, len(tuple_a)):
-            a.append(tuple_a[i]+tuple_b[i])
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
@@ -14,6 +11,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-        for i in range(0, len(tuple_a)):
-            a.append(tuple_a[i]+tuple_b[i])
+    for i in range(0, len(tuple_a)):
+        a.append(tuple_a[i]+tuple_b[i])
     return (tuple(a))
