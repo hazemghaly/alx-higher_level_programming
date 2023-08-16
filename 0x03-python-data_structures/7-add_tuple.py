71eaa8665ec2ebb681e0584e008a4a1d1de24c96
hazemghaly <hazemghaly30@gmail.com>
2023-08-16 11:46:09 +0000

    print add of python tulpes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     a = []
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
     if len(tuple_b) < 2:
         if len(tuple_b) == 0:
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-    for i in range(len(tuple_a)):
-        a.append(tuple_a[i]+tuple_b[i])
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) == 3 or len(tuple_a) == 3:
+        for i in range(len(tuple_a) - 1):
+            a.append(tuple_a[i]+tuple_b[i])
+    else:
+        for i in range(len(tuple_b)):
+            a.append(tuple_a[i]+tuple_b[i])
     return (tuple(a))
