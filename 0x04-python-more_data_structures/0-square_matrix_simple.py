e2a21e633aed99159033dcbe2628ac04da087621
hazemghaly <hazemghaly30@gmail.com>
2023-08-16 16:43:21 +0000

    print square of matrix element

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     cpy_max = list(map(list, matrix))
-    max = []
+    m = []
     sq = []
     for i in range(len(cpy_max)):
         for j in range(len(cpy_max)):
-            max.append(cpy_max[i][j] ** 2)
-    sq = [[max[(j + 1) * x + y] for y in range(j + 1)] for x in range(i + 1)]
-    return (sq)
+            m.append(cpy_max[i][j] ** 2)
+    if i == j:
+        sq = [[m[(j + 1) * x + y] for y in range(j + 1)] for x in range(i + 1)]
+        return (sq)
+    else:
+        return(matrix)
