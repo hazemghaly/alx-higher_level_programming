a14e128c87278b22e21e2aaa8956b4b511780c06
hazemghaly <hazemghaly30@gmail.com>
2023-08-16 11:13:21 +0000

    print rep;laced elemeent of python list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
-        return (my_list)
     cpy_list = []
     for s in range(len(my_list)):
         cpy_list.append(my_list[s])
-    cpy_list[idx] = element
-    return (cpy_list)
+    if idx >= 0 and idx < len(my_list):  
+        cpy_list[idx] = element
+        return (cpy_list)
+    else:
+        return(cpy_list)
