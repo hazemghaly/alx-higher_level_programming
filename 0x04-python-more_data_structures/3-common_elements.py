aee8390bf43947136229e56653972214d61eea16
hazemghaly <hazemghaly30@gmail.com>
2023-08-16 22:58:44 +0000

    show common element

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,5 +3,5 @@ def common_elements(set_1, set_2):
     my_set = list(set(set_1))
     my_set2 = list(set(set_2))
     for i in range(len(my_set)):
-    if my_set[i] in my_set2:
-        return (list(str(my_set[i])))
+        if my_set[i] in my_set2:
+            return (list(str(my_set[i])))
