e459000e1f671293e283c4f6cd21943ed10604ae
hazemghaly <hazemghaly30@gmail.com>
2023-08-16 11:31:30 +0000

    print no C  of python list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,10 +3,12 @@ def no_c(my_string):
     list_string = list(my_string)
     _string = list_string.copy()
     p = ""
-    if 'c' in list_string:
-        _string.remove('c')
-    if 'C' in list_string:
-        _string.remove('C')
+    for i in range(len(list_string)):
+        if "c" or "C" in _string:
+            if 'c' in _string:
+                _string.remove('c')
+            if 'C' in _string:
+                _string.remove('C')
     for i in range(len(_string)):
         p += "{}".format(*_string[i])
     return (p)
