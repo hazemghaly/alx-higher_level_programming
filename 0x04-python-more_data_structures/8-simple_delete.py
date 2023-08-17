dfb72594476aba540d2a1cef5a4e8f20985d6fa7
hazemghaly <hazemghaly30@gmail.com>
2023-08-17 00:50:33 +0000

    delet  sorted dict by keys dic.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,6 +2,6 @@
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         a_dictionary.pop(key)
-        return (a_dictionary)
-    else:
-        return(a_dictionary)
+    return (a_dictionary)
+#    else:
+ #       return(a_dictionary)
