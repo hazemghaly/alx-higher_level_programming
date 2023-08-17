a94b8ddd9b674375b01074da637ddd537f7e5c37
hazemghaly <hazemghaly30@gmail.com>
2023-08-17 02:19:52 +0000

    aquare matrix element.

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    r = len(matrix)
-    c = len(matrix[0])
-    cpy_max = list(map(list, matrix))
-    max = []
-    sq = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            max.append(cpy_max[i][j] ** 2)
-    sq = [[max[(c) * x + y] for y in range(c)] for x in range(r)]
-    return (sq)
+    return (list(map(lambda x :list(map(lambda y: (y**2), x)), matrix)))
