06a5fa5978996c2b21e1c7efa523a18bbaa98176
hazemghaly <hazemghaly30@gmail.com>
2023-08-17 13:34:31 +0000

    complex  element.

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    key = {i for i in a_dictionary if a_dictionary[i]== value}
+    for j in key:
+        if j in a_dictionary.keys():
+            a_dictionary.pop(j)
+    return (a_dictionary)
