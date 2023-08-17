5cb0c100e932fe95e7e29f575de6a2c6f1db695d
hazemghaly <hazemghaly30@gmail.com>
2023-08-17 01:50:05 +0000

    max by keys dic.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
-    return (max(k for k, v in a_dictionary.items()))
+        return (None)
+    b = max(k for k, v in a_dictionary.items())
+	return (b)
