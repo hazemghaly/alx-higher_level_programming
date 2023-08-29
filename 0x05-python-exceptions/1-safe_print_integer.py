496fa5b4857728071294b8f62fcee21c79cb0ddd
hazemghaly <hazemghaly30@gmail.com>
2023-08-29 01:42:57 +0000

    print intger elememt

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except (TypeError):
+    except(ValueError, TypeError, IndexError):
         return (False)
     return (True)
