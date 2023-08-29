54071bf894a2b144779809134f3ccf35eeec8dac
hazemghaly <hazemghaly30@gmail.com>
2023-08-29 17:30:37 +0000

    print exception int

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        print(e)
+        return (False)
+    return (True)
