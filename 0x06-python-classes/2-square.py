9ff5891ee195b9cd6e3b5bde5973814198f79200
hazemghaly <hazemghaly30@gmail.com>
2023-08-30 01:24:53 +0000

    classes square with private size with errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define class named Square """
+
+
+class Square:
+    """define class named Square """
+    def __init__(self, size=0):
+        """define privat size atturbute Square """
+        if not isinstance(size, int):
+            raise TypeError ("size must be an integer")
+        if size < 0:
+            raise ValueError ("size must be >= 0")
+        self.__size = size
