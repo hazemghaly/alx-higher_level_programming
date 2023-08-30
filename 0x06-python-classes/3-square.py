0a1c6b92e8e182d685a83530259b5552ddf9814b
hazemghaly <hazemghaly30@gmail.com>
2023-08-30 01:46:09 +0000

    classes square with area  with errors

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define class named Square """
+
+
+class Square:
+    """define class named Square """
+    def __init__(self, size=0):
+        """define privat size atturbute Square """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """define public aera atturbute """
+        return (self.__size ** 2)
