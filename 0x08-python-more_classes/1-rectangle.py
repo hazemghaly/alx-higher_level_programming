4b345f7a6cb91d4e7c1387dacfc54c8faaea9509
hazemghaly <hazemghaly30@gmail.com>
2023-09-04 22:57:08 +0000

    Private instance attribute class Rectangle .

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""define class named a rectangle """
+
+
+class Rectangle:
+    """difine class named a rectangle """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """define getter of atturbute """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """define setter of atturbute """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """define getter of atturbute """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """define setter of atturbute """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
