0dd55c2041c4709b671391170bf44166cb7bf342
hazemghaly <hazemghaly30@gmail.com>
2023-09-11 22:31:38 +0000

    Python - Inheritance returns the list of available attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define function return of available attributes and methods"""
+
+
+def lookup(obj):
+    """define function return of available attributes and methods"""
+    return (dir(obj))
