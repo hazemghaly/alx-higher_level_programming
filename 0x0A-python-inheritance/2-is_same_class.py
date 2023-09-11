38b4e448588d1870d3ef16f2d38cfb4157b1fad3
hazemghaly <hazemghaly30@gmail.com>
2023-09-11 23:25:01 +0000

    Python - Inheritance exactly an instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define function return of available attributes and methods"""
+
+
+def is_same_class(obj, a_class):
+    """ function returns type an instance of the specified class"""
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
