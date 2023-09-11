1ebefa53b23d394c9a5e25ea6f25a458252b1ca6
hazemghaly <hazemghaly30@gmail.com>
2023-09-11 23:51:00 +0000

    Python - Inheritance exactly an instance ofa class that inherited from,

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@
 def inherits_from(obj, a_class):
     """ function returns type an instance of the specified class"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
-       return (True)
+        return (True)
     return (False)
