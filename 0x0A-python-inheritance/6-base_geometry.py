f1246c106ccc34726a7c7ff15d017fb82fc63d39
hazemghaly <hazemghaly30@gmail.com>
2023-09-12 00:02:54 +0000

    Python - Inheritance class with area attribuates

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""difine class named a BaseGeometry """
+
+
+class BaseGeometry:
+    """difine class named a BaseGeometry """
+    def __init__(self):
+        """define atturbute """
+        pass
+
+    def area(self):
+        """define public area atturbute """
+        raise Exception("area() is not implemented")
