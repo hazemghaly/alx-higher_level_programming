77a0e67daaa0a9f5e6edf97280f444ad7d991b8c
hazemghaly <hazemghaly30@gmail.com>
2023-09-12 23:30:04 +0000

    Python - In/out open and read file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define function named write_file """
+
+
+def write_file(filename="", text=""):
+    """define return write_file """
+    with open("filename", 'w', encoding="utf-8") as file:
+        return(file.write(text))
