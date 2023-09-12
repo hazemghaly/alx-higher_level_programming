adf75210ab6c2a83d11fe4545b547fae965dbb71
hazemghaly <hazemghaly30@gmail.com>
2023-09-12 22:55:35 +0000

    Python - In/out open and read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""define function named add_attribute """
+
+
+def read_file(filename=""):
+    """define return add attributes """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())
