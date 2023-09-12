063657de9041ca207ffa41293867a95333547032
hazemghaly <hazemghaly30@gmail.com>
2023-09-12 02:19:38 +0000

    Python - Inheritance return reversed comp. oparetor

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,7 @@ class MyInt(int):
     """difine class named a MyInt """
     def __eq__(self, vlu):
         """define return reversed oparetor representation """
-        if(vlu == self.real):
-            return (self.real != vlu)
+        return (self.real != vlu)
 
     def __ne__(self, y):
         """define return reversed oparetor representation """
