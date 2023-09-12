c0458481136f37a33994b8adfb90def2fcca494b
hazemghaly <hazemghaly30@gmail.com>
2023-09-12 00:50:56 +0000

    Python - Inheritance Rectaongle class with area attribuates with error and tests

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""difine class named a BaseGeometry """
+
+
+class BaseGeometry:
+    """difine class named a BaseGeometry """
+
+    def integer_validator(self, name, value):
+        """define atturbute """
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError(f"{self.name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+    def area(self):
+        """define public area atturbute """
+        raise Exception("area() is not implemented")
+
+class Rectangle(BaseGeometry):
+    """difine class named a rectangle """
+
+    def __init__(self, width, height):
+        """define atturbute """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
