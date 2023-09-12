6c725e1c5304e17ec9e734729c40e2254b25a83e
hazemghaly <hazemghaly30@gmail.com>
2023-09-12 01:40:00 +0000

    Python - Inheritance  class Square that inherits from Rectangle with str reper

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""difine class named a BaseGeometry """
+
+
+class BaseGeometry:
+    """difine class named a BaseGeometry """
+
+    def integer_validator(self, name, value):
+        """define atturbute """
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    def area(self):
+        """define public area atturbute """
+        raise Exception("area() is not implemented")
+
+
+class Rectangle(BaseGeometry):
+    """difine class named a rectangle """
+
+    def __init__(self, width, height):
+        """define atturbute """
+        super().integer_validator(width, height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """define public area atturbute """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """define return str representation """
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle, BaseGeometry):
+    """difine class named a rectangle """
+
+    def __init__(self, size):
+        """define atturbute """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """define public area atturbute """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """define return str representation """
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
