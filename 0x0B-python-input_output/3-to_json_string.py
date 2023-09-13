e2bde8489a66969b13bc33e9d3325c629449aede
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 00:35:58 +0000

    Python - In/out json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define function named to_json_string """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """define return to_json_string """
+    return (json.dumps(my_obj))
