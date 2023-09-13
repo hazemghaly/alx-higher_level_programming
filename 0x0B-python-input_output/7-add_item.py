3ad27b13e990f6e897247a0722d0a14aa323b307
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 01:42:44 +0000

    Python - In/out add to json to file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define function named add from_json_string """
+
+
+import sys
+
+if __name__ == "__main__":
+
+    save_to_json_file = __import__('b').save_to_json_file
+    load_from_json_file = __import__('b').load_from_json_file
+    try:
+        object = load_from_json_file("add_item.json")
+    except Exception:
+        object = []
+    object.extend(list(sys.argv[1:]))
+    save_to_json_file(object, "add_item.json")
