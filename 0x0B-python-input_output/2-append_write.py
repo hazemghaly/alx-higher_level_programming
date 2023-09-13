98ecacb40c650ecb8057482a5e3de9ba5cee94c5
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-09-13 16:18:28 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """define return write_file """
 
     with open(filename, 'a', encoding="utf-8") as file:
-        return(file.write(text))
+        return (file.write(text))
