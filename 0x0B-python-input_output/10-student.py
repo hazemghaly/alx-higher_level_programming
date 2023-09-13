509eda91b50a52fa606da414fe1914e61de42db4
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 02:32:52 +0000

    Python - In/out student class dict. repre.

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""define function return of available attributes and methods"""
+
+
+class Student:
+    """difine class that inherits from list:"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary represntation"""
+
+        try:
+            for a in attrs:
+                if type(a) is not str:
+                    return (self.__dict__)
+        except Exception:
+            return (self.__dict__)
+
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            h_dict = dict()
+            for k, vlu in self.__dict__.items():
+                if k in attrs:
+                    h_dict[k] = vlu
+            return (h_dict)
