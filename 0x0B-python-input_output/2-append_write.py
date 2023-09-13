d770f8055092f29823b6887e028ee6665841695f
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 00:26:33 +0000

    Python - In/out append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define function named write_file """
+
+
+def append_write(filename="", text=""):
+    """define return write_file """
+
+    with open(filename, 'a', encoding="utf-8") as file:
+        return(file.write(text))
