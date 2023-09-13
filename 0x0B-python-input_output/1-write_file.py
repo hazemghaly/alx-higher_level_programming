57cd4e95733d543326a23dd78fa6000db181180b
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 00:24:13 +0000

    Python - In/out open and read file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """define return write_file """
-    with open("filename", 'w', encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return(file.write(text))
