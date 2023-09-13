27c260974a9cd4291054907eaef1807b390ea0a9
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 01:46:16 +0000

    Python - In/out add to json to file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,11 @@
 
 import sys
 
-if __name__ == "__main__":
-
-    save_to_json_file = __import__('b').save_to_json_file
-    load_from_json_file = __import__('b').load_from_json_file
-    try:
-        object = load_from_json_file("add_item.json")
-    except Exception:
-        object = []
-    object.extend(list(sys.argv[1:]))
-    save_to_json_file(object, "add_item.json")
+save_to_json_file = __import__('b').save_to_json_file
+load_from_json_file = __import__('b').load_from_json_file
+try:
+    object = load_from_json_file("add_item.json")
+except Exception:
+    object = []
+object.extend(list(sys.argv[1:]))
+save_to_json_file(object, "add_item.json")
