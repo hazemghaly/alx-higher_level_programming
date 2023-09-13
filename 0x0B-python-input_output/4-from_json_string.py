2ed506074ffee427c26027f8486f314b33758dfc
hazemghaly <hazemghaly30@gmail.com>
2023-09-13 00:41:30 +0000

    Python - In/out not json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define function named from_json_string """
+
+
+import json
+
+
+def from_json_string(my_str):
+    """define return from_json_string """
+    return (json.loads(my_str))
