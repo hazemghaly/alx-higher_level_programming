5df0dc9dde426467d2f99c05b6d6e0dcefdede6e
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-09-13 16:18:48 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """define return write_file """
     with open(filename, "w", encoding="utf-8") as file:
-        return(file.write(text))
+        return (file.write(text))
