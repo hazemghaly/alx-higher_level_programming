7a9bdd1f5b17a1e27d56022d750bdf61538d97f1
hazemghaly <hazemghaly30@gmail.com>
2023-09-18 17:47:09 -0700

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """define getter of atturibute """
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """define setter of atturbute """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """define public overriding  atturbute """
         return ("[Square] ({}) {}/{} - {}".format
