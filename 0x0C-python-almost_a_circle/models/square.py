89c1d48d8116241eb0e7f854485edd6d06fbb6ef
hazemghaly <hazemghaly30@gmail.com>
2023-09-18 17:42:26 -0700

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,10 +8,9 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """difine class named a rectangle """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
-
-def __str__(self):
+    def __str__(self):
         """define public overriding  atturbute """
         return ("[Square] ({}) {}/{} - {}".format
                 (self.id, self.x, self.y, self.width))
