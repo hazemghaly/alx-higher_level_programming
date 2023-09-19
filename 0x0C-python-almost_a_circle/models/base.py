c15d2894206eda59b5c88e003ba8bdb75613b198
hazemghaly <hazemghaly30@gmail.com>
2023-09-18 18:57:09 -0700

    json basee

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """define return to_json_string """
-        if list_dictionaries is None or len(list_dictionaries) == []:
+        if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
         return (json.dumps(list_dictionaries))
 
