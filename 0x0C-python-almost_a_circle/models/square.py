e37757ca9c2504d02c94fd06edfc587bfce28ecb
hazemghaly <hazemghaly30@gmail.com>
2023-09-18 17:34:25 -0700

    Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""define class named a Square """
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """difine class named a rectangle """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, x, y, size, size)
+
+
+def __str__(self):
+        """define public overriding  atturbute """
+        return ("[Square] ({}) {}/{} - {}".format
+                (self.id, self.x, self.y, self.width))
