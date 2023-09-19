fb519a0a3f28d35da22cffcbd8112c64fa5d4c5f
hazemghaly <hazemghaly30@gmail.com>
2023-09-18 17:52:02 -0700

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,34 @@ class Square(Rectangle):
         """define public overriding  atturbute """
         return ("[Square] ({}) {}/{} - {}".format
                 (self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """define public argument  atturbute """
+        if args and len(args) != 0:
+            i = 1
+            for arg in args:
+                if i == 1:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 2:
+                    self.size = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+                i += 1
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
