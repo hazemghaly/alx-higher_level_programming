4ca77a3e48816286a3f58762b20aa2d2ba6fb3ca
hazemghaly <hazemghaly30@gmail.com>
2023-10-19 06:37:25 -0700

    DB

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- create tabels data base
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    name VARCHAR(256) NOT NULL,
-     id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT
-     );
+    PRIMARY KEY(id),
+    id INT UNIQUE NOT NULL  AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+    );
