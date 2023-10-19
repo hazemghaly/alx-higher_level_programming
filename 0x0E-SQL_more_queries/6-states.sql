8959e490eac35a2311914474786d13ba19c74989
hazemghaly <hazemghaly30@gmail.com>
2023-10-19 05:53:04 -0700

    DB

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create tabels data base
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    name VARCHAR(256) NOT NULL,
+     id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT
+     );
