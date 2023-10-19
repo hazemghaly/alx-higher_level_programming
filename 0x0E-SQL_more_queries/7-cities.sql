6758f5cc55780da3d229b34e5f1e7cb5acb51c14
hazemghaly <hazemghaly30@gmail.com>
2023-10-19 06:00:43 -0700

    DB

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- create tabels data base
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL, PRIMARY KEY(id),
      id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
      state_id INT NOT NULL,
      FOREIGN KEY (state_id)
