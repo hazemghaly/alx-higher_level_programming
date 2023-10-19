9ae09f7ce5f4f0b8bacee4da2e966112b0eb5f11
hazemghaly <hazemghaly30@gmail.com>
2023-10-19 05:25:24 -0700

    DB

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- script that lists all privileges of the MySQL users
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
