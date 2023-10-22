b458e307efb7e4361139d12d162b88a2d3557d80
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-10-23 02:16:03 +0300

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,5 @@
 -- create tabels data base
 SELECT city, AVG(value) As avg_temp
-FROM temperatures
-WHEN month = "7" and month = "8"
-GROUP BY `city`
-ORDER BY `avg_temp` DESC
-LIMIT 2;
+FROM temperatures WHEN month = "7" and month = "8"
+GROUP BY city ORDER BY avg_temp DESC
+LIMIT 3;
