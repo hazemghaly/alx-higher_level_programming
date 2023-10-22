166e9a6a720f1302741ed8f9b0652c07234accd0
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-10-23 02:13:49 +0300

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
 -- create tabels data base
-SELECT city, max(temperature) As max_temp
+SELECT city, max(value) As max_temp
 FROM temperatures
 WHEN month = "7" and month = "8"
 GROUP BY `city`
-ORDER BY `avg_temp` DESC;
+ORDER BY `avg_temp` DESC
+LIMIT 2;
