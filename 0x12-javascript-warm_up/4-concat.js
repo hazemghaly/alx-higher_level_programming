bf74358fe8967dce421360a14b5dee6e1d90bc1f
hazemghaly <hazemghaly30@gmail.com>
2023-11-14 13:12:59 -0800

    java task 1 late

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,18 +1,2 @@
 #!/usr/bin/node
-const { argv, exit } = require('node:process');
-let i = 2;
-if (!argv[2]) {
-  console.log('undefined is undefined \r');
-}
-while (argv[i]) {
-  if (!argv[2]) {
-    console.log(' is undefined \r');
-  } else {
-    i++;
-    console.log('%s is %s \r', argv[2], argv[i]);
-  }
-  i++;
-  if (!argv[i]) {
-    exit();
-  }
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
