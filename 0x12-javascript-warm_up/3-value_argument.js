ee40783f1dce8bb1debf247315c3af8ab40c3b1a
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-14 23:18:11 +0200

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-const { argv, exit } = require('node:process');
-if (!argv[2]) {
-  console.log('No argument');
-  exit();
-}
-let i = 2;
-if (!argv[i]) {
-  exit();
-}
-while (argv[i]) {
-  console.log(process.argv[i]);
-  i++;
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
