78d939ffc78ca322a41b84ac333efde9e7494c15
hazemghaly <hazemghaly30@gmail.com>
2023-11-14 12:08:41 -0800

    java task 1 late

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (!argv[2]) { console.log('No argument'); } else {
-  for (let i = 2; s = process.argv[i]; i++) {
-    console.log(s);
-  }
+const { argv, exit } = require('node:process');
+if (!argv[2]) {
+  console.log('No argument');
+  exit();
+}
+let i = 2;
+if (!argv[i]) {
+  exit();
+}
+while (argv[i]); {
+  console.log(process.argv[i]);
+  i++;
 }
