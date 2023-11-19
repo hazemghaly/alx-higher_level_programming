615b2c8a91213e92c96dbc6f02278f584339d6e1
hazemghaly <hazemghaly30@gmail.com>
2023-11-19 13:49:37 -0800

    SQL python

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,9 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states')
-    states = cursor.fetchall()
-    for state in states:
-        for a in state:
-            print(state)
+    states_starting_with_n = cursor.fetchall()
+    for state in states_starting_with_n:
+        print(state)
     cursor.close()
     db.close()
