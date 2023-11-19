8210c26caeadcf04458003f3df0eeeb16c96f585
hazemghaly <hazemghaly30@gmail.com>
2023-11-19 13:43:18 -0800

    SQL python

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Write a script that lists all states from the database hbtn_0e_0_usa:'''
+
+
+import sys
+import MySQLdb
+''' Module For Connecting To MySQL database'''
+
+if __name__ == "__main__":
+    a = "N"
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+    states = cursor.fetchall()
+    for state in states:
+        for a in state:
+            print(state)
+    cursor.close()
+    db.close()
