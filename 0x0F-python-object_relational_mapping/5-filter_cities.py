24fbc92cfa347640d0a154d9d538603e24e9f8b7
hazemghaly <hazemghaly30@gmail.com>
2023-11-20 05:31:58 -0800

    SQL python

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     query = '''
     SELECT cities.name 
     FROM cities
-    JOIN states ON cities.state_id = states.id
+    LEFT JOIN states ON cities.state_id = states.id
     WHERE states.name Like %s
     '''
     cursor.execute(query, (sys.argv[4] + '%',))
