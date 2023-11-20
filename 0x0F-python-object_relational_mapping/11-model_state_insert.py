804b40f41461ded756d97e1058962e405aeaa40f
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-21 00:16:10 +0200

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,11 +9,17 @@ from model_state import Base, State
 ''' Module For Connecting To MySQL database'''
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = Session(engine)
-    session.add(State(name="Louisiana"))
-    session.commit()
-    session.close()
+    try:
+        engine = create_engine(
+            'mysql://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        session = Session(engine)
+        session.add(State(name="Louisiana"))
+        session.commit()
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        session.close()
