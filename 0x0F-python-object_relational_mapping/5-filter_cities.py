8224f75ebcf07db9026aa4dc70f3fb14ceb680e5
hazemghaly <hazemghaly30@gmail.com>
2023-11-20 05:25:40 -0800

    SQL python

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Write a script that lists all states from the database hbtn_0e_0_usa:'''
+
+
+import sys
+import MySQLdb
+''' Module For Connecting To MySQL database'''
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = '''
+    SELECT cities.id, cities.name, COALESCE(states.name, "No state")
+    FROM cities 
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name Like %s
+    '''
+    cursor.execute(query, (sys.argv[4] + '%',))
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
+    cursor.close()
+    db.close()
