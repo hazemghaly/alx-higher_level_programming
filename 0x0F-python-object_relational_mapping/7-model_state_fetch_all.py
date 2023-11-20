12887fb1b12aaedc4a5117a06a9a0f79bc65a561
hazemghaly <hazemghaly30@gmail.com>
2023-11-20 13:07:40 -0800

    SQL python

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Write a script that lists all states from the database hbtn_0e_0_usa:'''
+
+
+import sys
+import MySQLdb
+from model_state import Base, State
+''' Module For Connecting To MySQL database'''
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
