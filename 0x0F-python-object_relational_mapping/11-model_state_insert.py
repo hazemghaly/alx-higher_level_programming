f9b25719a96c3b86113c4ec3697287b3419c6cdd
hazemghaly <hazemghaly30@gmail.com>
2023-11-20 14:12:27 -0800

    SQL python

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Write a script that lists all states from the database hbtn_0e_0_usa:'''
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+''' Module For Connecting To MySQL database'''
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    session.add(State(name="Louisiana"))
+    session.commit()
+    session.close()
