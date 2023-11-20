8efccae27b65c007c0e207f6eef5940cf19b62df
hazemghaly <hazemghaly30@gmail.com>
2023-11-20 04:22:12 -0800

    SQL python

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Write a script that lists all states from the database hbtn_0e_0_usa:'''
+
+
+import sys
+import MySQLdb
+''' Module For Connecting To MySQL database'''
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute
+    ('SELECT * FROM cities INNER JOIN states ON cities.state_id = states.id')
+    cities = cursor.fetchall()
+    for city in (cities):
+        print(city)
+    cursor.close()
+    db.close()
