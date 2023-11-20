63f28063d02c887e73af33fbb017bbd98312293e
hazemghaly <hazemghaly30@gmail.com>
2023-11-20 14:38:57 -0800

    SQL python

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    state_to_update = session.query(State).filter_by(id = 2).first()
+    state_to_update = session.query(State).filter_by(id=2).first()
     state_to_update.name = "New Mexico"
     session.commit()
     session.close()
