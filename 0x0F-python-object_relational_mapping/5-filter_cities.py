252a7bd757d7ff3f83ef1c734f3895e58ef88274
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-21 10:12:07 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     query = '''
-    SELECT COALESCE(cities.name, 'No cities')
+    SELECT cities.name, COALESCE(states.name, "No state")
     FROM cities
     LEFT JOIN states ON cities.state_id = states.id
     WHERE states.name Like %s
