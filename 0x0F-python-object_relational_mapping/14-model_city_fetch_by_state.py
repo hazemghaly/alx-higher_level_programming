476006704baead4a04a18de5837b58e7ac4861d2
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-21 10:35:57 +0200

    Update 14-model_city_fetch_by_state.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Write a script that lists all states from the database hbtn_0e_0_usa:'''
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+
+''' Module For Connecting To MySQL database'''
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    st = session.query(State.name, City.id, City.name).join(
+        City, State.id == City.state_id).all()
+    for State.name, City.id, City.name in st:
+        print("{}: ({}) {}".format(State.name, City.id, City.name))
+    session.close()
