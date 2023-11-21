9609adc21094c71020dada4d186b60d14273fb65
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-11-21 13:55:41 +0200

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    citys = session.query(City).all()
-    for city in citys:
-        print("{}: {}".format( city.state.id, city.state.name))
-        for scity in city.state.cities:
-            print("\t{}: {}".format(scity.id, scity.name))
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
     session.close()
