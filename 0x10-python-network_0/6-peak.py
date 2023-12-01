6ee0146a669349138b9e92ff1ede28721a9e3cd2
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-01 16:44:52 +0200

    Update 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" find_peak """
+
+def find_peak(list_of_integers):
+    '''function'''
+    if len(list_of_integers) == 0:
+        return None
+    start = 0
+    end = len(list_of_integers) - 1
+    while start < end:
+        mid = (start + end) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    if start == len(list_of_integers) - 1:
+        return list_of_integers[start]
+    return list_of_integers[start]
