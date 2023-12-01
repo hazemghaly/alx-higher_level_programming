073591c3a888326787461c38e97d843818f7ed9c
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-01 04:49:51 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post
-curl -X POST -d "email: test@gmail.com&subject: I will always be here for PLD" -s "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" -s "$1"
