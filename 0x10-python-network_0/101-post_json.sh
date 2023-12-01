09531d69b00350cb479f5626f1cf844d596bcebd
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-01 05:02:29 +0200

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #json
-curl -i -s "$1" -X POST -H "Content-Type: application/json" -o "$2" -H "Accept: application/json"
+curl -i -s "$1" -d "$2" -X POST -H "Content-Type: application/json" -H "Accept: application/json"
