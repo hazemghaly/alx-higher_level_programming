c60526bbfd913e928cb9b266db384aa1a18322b8
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 18:31:07 +0200

    Update 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''a Python script for pst email requests'''
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {'email': sys.argv[2]}
+
+    req = requests.post(sys.argv[1], data)
+    print(req.read())
