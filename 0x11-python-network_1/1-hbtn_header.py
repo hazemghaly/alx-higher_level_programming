f10c437bcf440e16601d86569944abc90628c999
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 16:23:39 +0200

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@
 import urllib.request
 import sys
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
