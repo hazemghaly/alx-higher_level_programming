6ce514caf6b1422ecb79cd4b613ed6e957bd2c52
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 18:17:44 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 
 req = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
-print("\t- type: {}".format(type(req)))
-print("\t- content: {}".format(req))
+print("\t- type: {}".format(type(req.text)))
+print("\t- content: {}".format(req.text))
