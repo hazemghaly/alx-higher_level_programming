36398e3718e73f16c995a7c104f2f27d2a58edeb
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 18:00:36 +0200

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
             print(response.read().decode('utf-8'))
 
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e}")
+        print(f"Error code: {e.code}")
