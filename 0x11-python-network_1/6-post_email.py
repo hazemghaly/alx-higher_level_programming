b6b88fb9ecad8e5f473ffb208684ec7d8ae9ebbd
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 18:35:21 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     data = {'email': sys.argv[2]}
 
-    req = requests.post(sys.argv[1], data )
+    req = requests.post(sys.argv[1], data)
     print(req.text)
