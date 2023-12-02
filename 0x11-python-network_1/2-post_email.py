4e9263eb2b9c21d8a2fb7020de75a801a540b19b
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 16:56:22 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-'''a Python script for post mail'''
-
-
-import urllib.request
-import sys
-
-if __name__ == "__main__":
-    email = sys.argv[2]
-
-    req = urllib.request.Request(
-        sys.argv[1], email.encode('utf-8'), method="POST")
-    with urllib.request.urlopen(req) as response:
-        print(f"Your email is: {response.read().decode('utf-8')}")
-=======
 #!/usr/bin/python3
 '''a Python script for post mail'''
 
@@ -28,4 +12,3 @@ if __name__ == "__main__":
         sys.argv[1], email.encode('utf-8'), method="POST")
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
->>>>>>> cf0a1997fc0d19f35fd4dd9b5374357be407792d
