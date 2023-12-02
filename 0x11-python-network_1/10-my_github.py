5fcd9002404f9da6cd04f9432214d56c3c479356
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 19:52:42 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,12 +9,8 @@ if __name__ == "__main__":
 
     username = sys.argv[1]
     token = sys.argv[2]
-    # headers = {
-    # "Accept": "application/vnd.github.v3+json",
-    # "Authorization" : f"token {token}",
-    # }
     login = requests.get(
-        'https://api.github.com/search/repositories?q=github+api',
+        'https://api.github.com/user',
         auth=(username, token))
     res = login.json()
     print("{}".format(res.get("id")))
