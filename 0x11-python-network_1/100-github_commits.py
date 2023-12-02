812173b142c5314597e0c979c35a9043439227d1
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 22:30:56 +0200

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits/'.format(
         sys.argv[2], sys.argv[1])
     commits = requests.get(url).json(
-
     try:
         for commit in commits[:10]:
             sha = commit["sha"]
