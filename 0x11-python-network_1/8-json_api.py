aaa7ad5ef118dfebd344c8e95dcf558819d4a308
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 19:19:38 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,12 +7,12 @@ import sys
 
 if __name__ == "__main__":
 
-    if not sys.argv[1]:
-        letter = ""
-        payload = {'q': ""}
-    else:
+    if len(sys.argv) == 1:
         letter = sys.argv[1]
         payload = {'q': letter}
+    else:
+        letter = ""
+        payload = {'q': ""}
     req = requests.post('http://0.0.0.0:5000/search_user', params=payload)
 
     try:
