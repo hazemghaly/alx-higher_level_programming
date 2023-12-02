3d1556613f12d9f4653dc0c156fb33f52bb91821
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 19:44:25 +0200

    Update 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''a Python script requests'''
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    token = sys.argv[2]
+    # headers = {
+    # "Accept": "application/vnd.github.v3+json",
+    # "Authorization" : f"token {token}",
+    # }
+    login = requests.get(
+        'https://api.github.com/search/repositories?q=github+api',
+        auth=(username, token))
+    print(login(id))
