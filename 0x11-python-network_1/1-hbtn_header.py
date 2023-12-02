bf855c55386c512a1a90cdae6f9768620406b6c3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 16:25:41 +0200

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-'''a Python script for header'''
-
-
-import urllib.request
-import sys
-
-if __name__ == "__main__":
-
-    req = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(req) as response:
-        print(dict(response.headers).get("X-Request-Id"))
-=======
 #!/usr/bin/python3
 '''a Python script for header'''
 
@@ -23,4 +9,3 @@ import sys
 req = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(req) as response:
     print(dict(response.headers).get("X-Request-Id"))
->>>>>>> 61e3e1bc1485b6e3cc9f3dc882cdd5d400c589fa
