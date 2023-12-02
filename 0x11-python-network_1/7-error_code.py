7d0fb20ed8e6a8fe6866ba5f68a86c27baec1839
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 18:46:02 +0200

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 
     req = requests.get(sys.argv[1])
     if req.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
