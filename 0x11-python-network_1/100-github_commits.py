cddd3496425d98d297a3fa399be0d0b7372aabe1
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 22:47:16 +0200

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         sys.argv[2], sys.argv[1])
     commits = requests.get(url).json()
     try:
-        for commit in commits[:10]:
-            sha = commit.get("sha")
-            author_name = commit.get("commit", {}).get("author", {}).get("name", "Unknown")
-            print("{}: {}".format(sha, author_name))
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
