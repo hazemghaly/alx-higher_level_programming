6d87fde2baf847203b078989d1d37eeacc5a4d11
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 22:21:55 +0200

    Update 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = ' https://developer.github.com/v3/repos/{}/{}/commits/'.format(
+        sys.argv[2], sys.argv[1])
+    commits = requests.get(url).json()
+    try:
+        for commit in commits[:10]:
+            sha = commit["sha"]
+            author_name = commit["commit"]["author"]["name"]
+            print(f"{sha}: {author_name}")
+    except IndexError:
+        pass
