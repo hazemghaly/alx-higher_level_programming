a66569f4912f3bfee9387bc45523ed307dddc93a
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2023-12-02 18:55:07 +0200

    Update 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''a Python script requests'''
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    w = {'q': ''}
+    req = requests.post('http://0.0.0.0:5000/search_user', params = w)
+    res = req.json()
+    print(f"[{res.id}] {res.name}")
